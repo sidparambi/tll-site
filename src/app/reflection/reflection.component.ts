7f84cafc1188c43f39401b2ed603f289b5445456
sidparambi <socialcodin@gmail.com>
2022-12-21 08:50:06 +0530

    capstone2

--- src/app/reflection/reflection.component.ts
+++ src/app/reflection/reflection.component.ts
@@ -12,6 +12,12 @@ export class ReflectionComponent implements OnInit {
   isShown:boolean=false;
   isShown4:boolean=false;
   isShown5:boolean=false;
+  isShown6:boolean=false;
+  isShown7:boolean=false;
+
+  link1="./assets/badgr_main_page.png";
+  link2="./assets/external_criteria_1.png";
+  link3="./assets/external_criteria_2.png";
 
   constructor() { }
 
@@ -30,32 +36,57 @@ export class ReflectionComponent implements OnInit {
       this.isShown3 = false;
       this.isShown4 = false;
       this.isShown5 = false;
+      this.isShown6 = false;
+      this.isShown7 = false;
     }else if(button_num==2){
       this.isShown2 = ! this.isShown2;
       this.isShown1 = false;
       this.isShown3 = false;
       this.isShown4 = false;
       this.isShown5 = false;
+      this.isShown6 = false;
+      this.isShown7 = false;
     }else if(button_num==3){
       this.isShown3 = ! this.isShown3;
       this.isShown1 = false;
       this.isShown2 = false;
       this.isShown4 = false;
       this.isShown5 = false;
+      this.isShown6 = false;
+      this.isShown7 = false;
     }else if(button_num==4){
       this.isShown4 = ! this.isShown4;
       this.isShown1 = false;
       this.isShown2 = false;
       this.isShown3 = false;
       this.isShown5 = false;
+      this.isShown6 = false;
+      this.isShown7 = false;
     }else if(button_num==5){
       this.isShown5 = ! this.isShown5;
       this.isShown1 = false;
       this.isShown2 = false;
       this.isShown3 = false;
       this.isShown4 = false;
+      this.isShown6 = false;
+      this.isShown7 = false;
+    }else if(button_num==6){
+      this.isShown6 = ! this.isShown6;
+      this.isShown1 = false;
+      this.isShown2 = false;
+      this.isShown3 = false;
+      this.isShown4 = false;
+      this.isShown5 = false;
+      this.isShown7 = false;
+    }else if(button_num==7){
+      this.isShown7 = ! this.isShown7;
+      this.isShown1 = false;
+      this.isShown2 = false;
+      this.isShown3 = false;
+      this.isShown4 = false;
+      this.isShown5 = false;
+      this.isShown6 = false;
     }
-    
     console.log('shown1: '+this.isShown1);
     console.log('shown2: '+this.isShown2);
     console.log('shown3: '+this.isShown3);
